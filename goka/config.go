a633a0a21e14cf98cb0ae6a87bcac887730903a4
oxymoron0 <seyoumoon@gmail.com>
2025-06-08 18:53:04 +0900

    feat(config): add AppConfig struct and LoadConfig function

---
+++ goka/config.go
@@ -0,0 +1,41 @@
+package goka
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// AppConfig 구조체 정의
+type AppConfig struct {
+	Brokers []string `yaml:"brokers"`
+	Topic   string   `yaml:"topic"`
+	Group   string   `yaml:"group"`
+	SASL    struct {
+		Enable    bool   `yaml:"enable"`
+		User      string `yaml:"user"`
+		Password  string `yaml:"password"`
+		Mechanism string `yaml:"mechanism"`
+	} `yaml:"sasl"`
+	TLS struct {
+		Enable bool `yaml:"enable"`
+	} `yaml:"tls"`
+	Replication struct {
+		Table  int `yaml:"table"`
+		Stream int `yaml:"stream"`
+	} `yaml:"replication"`
+	Partitions int `yaml:"partitions"`
+}
+
+// LoadConfig 함수
+func LoadConfig(path string) (AppConfig, error) {
+	var c AppConfig
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return c, err
+	}
+	if err := yaml.Unmarshal(data, &c); err != nil {
+		return c, err
+	}
+	return c, nil
+}
